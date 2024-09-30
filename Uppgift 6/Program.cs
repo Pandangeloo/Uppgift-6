5069631d3b7b32eb92daef899d90c287bb0199e7
Angelica Norlander <angelicanorlander@telia.com>
2024-09-30 15:48:26 +0200

    Koden som vi utgår från

--- Uppgift 6/Program.cs
+++ Uppgift 6/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Uppgift_6
+﻿namespace NameSorter
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> names = new List<string> { "Anna", "John", "Alice", "Berit" };
+            Console.WriteLine("Original List:");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+
+            }
+
+            names.Sort(); //Sortera alfabetiskt
+            Console.WriteLine("\nSorted list:");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Enter name to search");
+            string searchName = Console.ReadLine();
+
+            if (names.Contains(searchName))
+            {
+                Console.WriteLine($"{searchName} is in the list");
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} is not in the list")
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
